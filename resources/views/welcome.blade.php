2f14ba3a352f008301b1d00923dea9b68ef8ca38
Mike Aldama <mikefantas2@gmail.com>
2019-03-07 13:04:02 -0600

    Se creó rama develop y cambio en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel MM
                 </div>
 
                 <div class="links">
