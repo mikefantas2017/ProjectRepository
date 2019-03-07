d328b7982c2b80693d894c60968dae7c26eaf2f7
Mike Aldama <mikefantas2@gmail.com>
2019-03-07 13:35:42 -0600

    Nuevo cambio en master

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,4 +11,5 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     // Este es un comentario extra.
+    // Nuevo cambio en Controller.
 }
