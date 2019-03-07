dc9bc2f33ea57a744ab4211c6ac6ac0f7ccc48f6
Mike Aldama <mikefantas2@gmail.com>
2019-03-07 13:13:49 -0600

    Nuevo cambio en welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel MM
+                    Laravel MM M2
                 </div>
 
                 <div class="links">
